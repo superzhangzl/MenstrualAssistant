14d7478c0b1e203c2967ec216ba22351ffa5d7f6
553234736 <z553234736@outlook.com>
2019-05-28 23:02:47 +0800

    add

---
+++ src/main/java/com/hd/menstrualassistant/MenstrualAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.hd.menstrualassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenstrualAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenstrualAssistantApplication.class, args);
+    }
+
+}
